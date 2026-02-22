0fb2c6129cd28c2ea401ef27f4610fe614711aa4
Adityaraje2001 <desaiadityaraje@gmail.com>
2026-02-22 17:22:57 +0530

    third commit

---
+++ src/logger.py
@@ -0,0 +1,16 @@
+import logger
+import os
+import sys
+from datetime import datetime
+
+LOG_FILE = f"log_{datetime.now().strftime('%Y-%m-%d')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logger.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logger.INFO
+)
